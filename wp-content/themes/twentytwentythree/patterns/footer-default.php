8f05ead9deb11bb0e27b4e9d56b0b695d562ab84
taychin <taychin@5lab.co>
2022-11-12 22:37:59 +0700

    test

--- wp-content/themes/twentytwentythree/patterns/footer-default.php
+++ wp-content/themes/twentytwentythree/patterns/footer-default.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Title: Default Footer
  * Slug: twentytwentythree/footer-default
@@ -7,19 +8,19 @@
  */
 ?>
 <!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group">
+<div class="wp-block-group" style="background:red ;">
 	<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|40"}}},"layout":{"type":"flex","justifyContent":"space-between"}} -->
 	<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--40)">
 		<!-- wp:site-title {"level":0} /-->
 		<!-- wp:paragraph {"align":"right"} -->
 		<p class="has-text-align-right">
-		<?php
-		printf(
-			/* Translators: WordPress link. */
-			esc_html__( 'Proudly powered by %s', 'twentytwentythree' ),
-			'<a href="' . esc_url( __( 'https://wordpress.org', 'twentytwentythree' ) ) . '" rel="nofollow">WordPress</a>'
-		)
-		?>
+			<?php
+			printf(
+				/* Translators: WordPress link. */
+				esc_html__('Proudly powered by %s', 'twentytwentythree'),
+				'<a href="' . esc_url(__('https://wordpress.org', 'twentytwentythree')) . '" rel="nofollow">WordPress</a>'
+			)
+			?>
 		</p>
 		<!-- /wp:paragraph -->
 	</div>
